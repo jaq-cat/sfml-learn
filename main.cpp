75073a671c38241ca200c04e42068ce022dc61bd
jaq-cat <jaq.cat@protonmail.ch>
2020-11-15 11:07:52 -0300

    key events

--- main.cpp
+++ main.cpp
@@ -1,22 +1,41 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::endl;
 
 #define WIDTH 1200
 #define HEIGHT 800
 
 int main() {
+    // create window
     sf::RenderWindow win(sf::VideoMode(WIDTH, HEIGHT), "Window",
             sf::Style::Titlebar);
     win.setPosition(sf::Vector2i(100, 100));
+    win.setVerticalSyncEnabled(true);
+
+    // initialization
     sf::CircleShape shape(100.0f);
     shape.setFillColor(sf::Color::Green);
 
     while (win.isOpen()) {
+        // event handling
         sf::Event ev;
         while (win.pollEvent(ev)) {
-            if (ev.type == sf::Event::Closed) {
-                win.close();
+            switch(ev.type) {
+                case sf::Event::Closed:
+                    win.close();
+                    break;
+                case sf::Event::KeyPressed:
+                    cout << ev.key.code << endl;
+                    break;
+                default:
+                    break;
+
             }
         }
+        // rendering
         win.clear();
         win.draw(shape);
         win.display();
